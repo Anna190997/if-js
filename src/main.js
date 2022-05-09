2298941e0ca846fcb935f0413fc08e53dce1b540
AnnaVerbilo <ann-verbilo@yandex.ru>
2022-05-09 11:58:37 +0300

    fixed function of change color

--- src/main.js
+++ src/main.js
@@ -11,31 +11,23 @@ export function number(a, b) {
 const sum = curry(number);
 console.log(sum(5)(2));
 
-export function setMagenta() {
-  this.style.color = 'magenta';
-  this.removeEventListener('click', setMagenta);
-  this.addEventListener('click', setCyan);
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+const first = document.getElementById('text1');
+const second = document.getElementById('text2');
+const third = document.getElementById('text3');
+
+const color = function () {
+  let i = 0;
+  return function () {
+    this.style.color = colors[i];
+    i++;
+    if (i >= colors.length) {
+      i = 0;
+    }
+  };
+};
+for (let i = 0; i < colors.length; i++) {
+  first.addEventListener('click', color());
+  second.addEventListener('click', color());
+  third.addEventListener('click', color());
 }
-export function setCyan() {
-  this.style.color = 'cyan';
-  this.removeEventListener('click', setCyan);
-  this.addEventListener('click', setFirebrick);
-}
-export function setFirebrick() {
-  this.style.color = 'firebrick';
-  this.removeEventListener('click', setFirebrick);
-  this.addEventListener('click', setSpringgreen);
-}
-export function setSpringgreen() {
-  this.style.color = 'springgreen';
-  this.removeEventListener('click', setSpringgreen);
-  this.addEventListener('click', setSkyblue);
-}
-export function setSkyblue() {
-  this.style.color = 'skyblue';
-  this.removeEventListener('click', setSkyblue);
-  this.addEventListener('click', setMagenta);
-}
-document.getElementById('text1').addEventListener('click', setMagenta);
-document.getElementById('text2').addEventListener('click', setMagenta);
-document.getElementById('text3').addEventListener('click', setMagenta);
