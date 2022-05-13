92a0877ee3b1c4752df6b1558ac6aaae85cf20cd
AnnaVerbilo <ann-verbilo@yandex.ru>
2022-05-13 19:34:27 +0300

    work with string

--- src/main.js
+++ src/main.js
@@ -1,9 +1,62 @@
-import { color } from './color.js';
+const date = '2020-11-26';
+const result = date.split('-').reverse().join('.');
+console.log(result);
 
-const first = document.getElementById('text1');
-const second = document.getElementById('text2');
-const third = document.getElementById('text3');
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
 
-first.addEventListener('click', color());
-second.addEventListener('click', color());
-third.addEventListener('click', color());
+function search(town) {
+  let city;
+  let country;
+  let hotel;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].city === town) {
+      country = data[i].country;
+      city = data[i].city;
+      hotel = data[i].hotel;
+      const place = `Страна: ${country}, ` + `город: ${city}, ` + `отель: ${hotel}`;
+      console.log(place);
+    }
+  }
+}
+search('Berlin');
