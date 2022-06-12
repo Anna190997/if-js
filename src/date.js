befdfd47ee07f0ca6187216f071cb8a02395f9ae
AnnaVerbilo <ann-verbilo@yandex.ru>
2022-06-12 17:46:00 +0300

    add contain of block Homes guests love from array, add slider

--- src/date.js
+++
@@ -1,3 +0,0 @@
-const date = '2020-11-26';
-export const result = date.split('-').reverse().join('.');
-console.log(result);
---
+++ src/js/slide.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-undef
+$(document).ready(function () {
+  $('.places_items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
