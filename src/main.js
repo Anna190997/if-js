e76e654c6df9567008e6ef00d5331aafc82b69ee
AnnaVerbilo <ann-verbilo@yandex.ru>
2022-07-15 23:23:03 +0300

    fixed

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
 const formEl = document.getElementById('form');
+const fileValue = document.getElementById('file')
 formEl.addEventListener('submit', async (event) => {
   event.preventDefault();
+  const file = fileValue.value;
+  const fd = new FormData();
+  fd.append('file', file);
 
   const fetchOptions = {
     method: 'POST',
-    body: new FormData(formEl),
+    body: fd,
     headers: {
       'Content-Type': 'multipart/form-data',
     },
     mode: 'no-cors',
   };
 
-  const res = await fetch('https://fe-student-api.herokuapp.com/api/file', fetchOptions)
+  const res = await fetch(`https://fe-student-api.herokuapp.com/api/file`, fetchOptions)
     .then((response) => {
       return response.json();
     })
     .then((result) => result)
     .catch((error) => console.log(error.message));
   console.log(res);
+  console.log(file);
+  console.log(fd);
 });
+
+
+
+
